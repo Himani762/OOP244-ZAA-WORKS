59c7d1597e42f210f413e34a08b05e3faa047d06
MinhPhamLapTrinh <116688660+MinhPhamLapTrinh@users.noreply.github.com>
2023-01-25 09:00:34 -0500

    WorkShop 2 first update

---
+++ WS01/DIY FIX/File.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <fstream>
+using namespace std;
+namespace sdds
+{
+    int main()
+    {
+        std::cout << "Hello world!";
+
+        return 0;
+        // string myFile;
+        // ifstream MyReadFile("students.csv");
+        // while (getline(MyReadFile, myFile))
+        // {
+        //     cout << myFile;
+        // }
+        // MyReadFile.close();
+        // return 0;
+    }
+}
+// 1. Read file and load data 
+// 2. Loop 
+//  2.1. cout "Enter GPA query..."
+//  2.2. Get user input
+
